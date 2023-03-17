96e8adc61f3279b538a003a27cbf50fc08429be3
Yago <peixinhoyago@gmail.com>
2023-03-16 22:27:56 -0300

    :construction: wip: Creating Veiaco Card

---
+++ src/components/common/VeiacoCard.js
@@ -0,0 +1,22 @@
+import avatarVeiacoCard from "../../assets/images/avatar-veiaco-card.svg";
+import phoneVeiaco from "../../assets/icons/phone-icon.svg";
+export default function VeiacoCard() {
+  return (
+    <div className="veiaco-card">
+      <div>
+        <span>
+          <img src={avatarVeiacoCard} alt="Temporary Veiaco user" />
+        </span>
+      </div>
+      <div className="veiaco-info">
+        <label>Yuri</label>
+        <label className="veiaco-label">
+          <span>
+            <img src={phoneVeiaco} alt="Phone icon" />
+          </span>
+          (83) 98616-5982
+        </label>
+      </div>
+    </div>
+  );
+}
--- src/pages/Veiacos/index.js
+++ src/pages/Veiacos/index.js
@@ -1,11 +1,21 @@
 import React from "react";
+import VeiacoCard from "../../components/common/VeiacoCard";
 import ListContentMainStructure from "../../components/layout/GenericOutletHeader";
+
+const dataVeiaco = [
+  { veiaco: "Yago" },
+  { veiaco: "Yuri" },
+  { veiaco: "Billie" },
+];
 
 export default function Veiacos() {
   return (
     <>
       <ListContentMainStructure pageTitle="Veiacos" />
-      <div>Listagem de veiacos!</div>
+
+      {dataVeiaco.map((veiaco) => (
+        <VeiacoCard />
+      ))}
     </>
   );
 }
