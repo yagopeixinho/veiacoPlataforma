471f158f4a8cd54cd6498b3bf9e6a20d9d3fd921
Yago <peixinhoyago@gmail.com>
2023-03-22 19:24:10 -0300

    :construction: wip: Working on creating Veiaco Dashboard layout

---
+++ src/pages/VeiacoDashboard/index.js
@@ -0,0 +1,10 @@
+import GenericOutletHeader from "../../components/layout/GenericOutletHeader";
+
+export default function VeiacoDashboard() {
+  return (
+    <div>
+      <GenericOutletHeader pageTitle="Dashboard" />
+      <div>Card do Veiaco</div>
+    </div>
+  );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import App from "../App";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Veiacos from "../pages/Veiacos";
+import VeiacoDashboard from "../pages/VeiacoDashboard";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
       {
         path: "/veiacos",
         element: <Veiacos />,
+      },
+      {
+        path: "/veiaco/:id/dashboard",
+        element: <VeiacoDashboard />,
       },
     ],
   },
