274cba44cb0abf70c32d63ff8c0c5b973bd74c34
Yago <peixinhoyago@gmail.com>
2023-03-29 17:28:58 -0300

    :sparkles: feat: Create veiaco screen

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -6,35 +6,25 @@ export default function TextInput({ label, classes = "", ...props }) {
 
   return (
     <div className={classes}>
-      <div className="input-block">
-        {props.iconleft && (
-          <img className="input-icon" src={props.iconleft} alt="teste" />
-        )}
-
-        {!props.mask ? (
+      <div>
+        {!props.mask && (
           <input
             className="input-text"
             {...field}
             {...props}
             placeholder={label}
           />
-        ) : (
-          <input
-            className="input-text"
-            {...field}
-            {...props}
-            alwaysShowMask={true}
-            placeholder={label}
-          />
-        )}
-        {props.loading && (
-          <i className="pi pi-spin pi-spinner input-loading"></i>
         )}
       </div>
+
       <div>
         {meta.touched && meta.error ? (
-          <div>{JSON.stringify(meta.error)}</div>
-        ) : null}
+          <div className="container-error">
+            <label className="error-label">{meta.error}</label>
+          </div>
+        ) : (
+          <div className="container-error" />
+        )}
       </div>
     </div>
   );
