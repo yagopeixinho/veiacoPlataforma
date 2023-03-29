c7583da2bb7aa572a9ecbdbb0d1a16ec3e691523
Yago <peixinhoyago@gmail.com>
2023-03-29 17:06:37 -0300

    :construction: wip: Veiaco Form Screen

--- src/pages/VeiacoForm/index.js
+++ src/pages/VeiacoForm/index.js
@@ -3,11 +3,9 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import ButtonSave from "../../components/common/ButtonSave";
 import TextInput from "../../components/common/TextInput";
-import GenericOutletHeader from "../../components/layout/GenericOutletHeader";
 import VeiacoService from "../../service/veiaco.service";
 import { veiacoSchema } from "../../validations/veiaco.validation";
 import avatarVeiacoCard from "../../assets/images/avatar-veiaco-card-1.png";
-import Button from "../../components/common/Button";
 
 export default function VeiacoForm() {
   const { id } = useParams();
@@ -61,52 +59,52 @@ export default function VeiacoForm() {
   }
 
   return (
-    <div>
-      <GenericOutletHeader
-        pageTitle="Adicionar veiaco"
-        inputSearchConfig={{ inputExist: false }}
-      />
-
-      <div>
-        <Formik
-          enableReinitialize={true}
-          validationSchema={veiacoSchema}
-          initialValues={veiacoFormInitialValues}
-          onSubmit={(values) => {
-            createVeiaco(values);
-          }}
-        >
-          {(props) => {
-            return (
-              <Form>
-                <div className="container-form-veiaco">
-                  <div className="veiaco-form-left">
-                    <div>
-                      <span>
-                        <img
-                          src={avatarVeiacoCard}
-                          alt="Temporary Veiaco user"
-                          className="veiaco-picture"
-                        />
-                      </span>
-                    </div>
+    <div className="veiaco-form">
+      <Formik
+        enableReinitialize={true}
+        validationSchema={veiacoSchema}
+        initialValues={veiacoFormInitialValues}
+        onSubmit={(values) => {
+          createVeiaco(values);
+        }}
+      >
+        {(props) => {
+          return (
+            <Form className="form-veiaco">
+              <div className="container-form-veiaco">
+                <div className="veiaco-form-left">
+                  <div>
+                    <h1 className="headline-new-veiaco">Novo veiaco</h1>
+                    <span>
+                      <img
+                        src={avatarVeiacoCard}
+                        alt="Temporary Veiaco user"
+                        className="veiaco-picture"
+                      />
+                    </span>
                   </div>
-                  <div className="veiaco-form-right">
+                </div>
+                <div className="veiaco-form-right">
+                  <h1>Informações básicas</h1>
+
+                  <div className="veiaco-form-grid">
                     <TextInput label="Nome" name="name" />
                     <TextInput label="E-mail" name="email" />
                     <TextInput label="Telefone" name="phone" />
                     <TextInput label="Cargo" name="occupation" />
+                  </div>
 
-                    <div className="veiaco-button-row">
+                  <div className="veiaco-button-row">
+                    <div className="btn-div">
                       <ButtonSave label="Salvar" type="submit" />
                     </div>
                   </div>
                 </div>
-              </Form>
-            );
-          }}
-        </Formik>
-      </div>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
